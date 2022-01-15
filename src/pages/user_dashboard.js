22915f50500e218fbc98724be25db66e48140273
Daniela Mazur <danielamazur2000@gmail.com>
2022-01-15 11:02:42 +0200

    Show user info on user dashboar

--- src/pages/user_dashboard.js
+++ src/pages/user_dashboard.js
@@ -1,4 +1,9 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useHistory } from "react-router-dom";
+import { Text, Avatar, Flex, Box } from "@chakra-ui/react";
+
+import { GetEmployeeByEmail } from "../services";
 
 import Calendar from "../components/CalendarComp";
 
@@ -9,25 +14,47 @@ import "../style/user_dashboard.css";
 import "../style/footer.css";
 
 export const UserDashboardPage = () => {
+  const history = useHistory();
+  const { user, isAuthenticated } = useAuth0();
+  const [employee, setEmployee] = useState(null);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      history.push("/");
+      return;
+    }
+    GetEmployeeByEmail(user.email).then((employee) => {
+      if (employee.data.id) {
+        setEmployee(employee.data);
+      }
+    });
+  }, [isAuthenticated, history]);
+
+  if (!user || !employee) {
+    return null;
+  }
+
   return (
     <div className="user-dashboard">
-      <div className="employee-info">
-        <div className="image">
-          <img src={UserAvatar} alt="UserAvatar" />
-        </div>
-        <div className="info">
-          <p className="employee-surname-name">Employee Name</p>
-          <p className="employee-surname-name">Employee Surame</p>
-          <p className="email-work-hours">Email: employee@org.com</p>
-          <p className="email-work-hours">Work Hours: xx</p>
-        </div>
-      </div>
+      <Flex background="blue.900" p={5}>
+        <Avatar size="2xl" name={user.name} src={user.picture || UserAvatar} />
+
+        <Flex ml={5} flexDirection="column" justifyContent="space-between">
+          <Text color="blue.50" fontSize="3xl">
+            {user.name}
+          </Text>
+          <Box>
+            <Text color="blue.50">Email: {user.email}</Text>
+            <Text color="blue.50">Work Hours: {employee.work_hours}</Text>
+          </Box>
+        </Flex>
+      </Flex>
       <div className="calendar">
         <Calendar />
       </div>
       <div className="footer">
         <div className="inf">
-          <img className="logo" src={Logo} />
+          <img className="logo" src={Logo} alt="company" />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim{" "}
