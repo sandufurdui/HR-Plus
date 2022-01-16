a89b886b75f1a4564142c5e344a6f0a2af0ff4b6
anastasiagh <gheorghita.anastasia@isa.utm.md>
2022-01-16 17:45:12 +0200

    Added lazy loading, changed calendar and replacement cost services

--- src/services/calendar.service.js
+++ src/services/calendar.service.js
@@ -1,7 +1,11 @@
 import { httpService } from "./http.service";
 
-const WorkHours = (calendar) => {
-  return httpService.post("calendars/", calendar);
+const WorkHours = (calendar, accessToken) => {
+  return httpService.post("calendars/", calendar, {
+    headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+  });
 };
 
 export { WorkHours };
